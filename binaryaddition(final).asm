0d22747f7d163c75997cee4728c63839e922f8b7
nayem9b <razwanniam1@gmail.com>
2024-09-07 06:12:56 +0600

    added comments

--- binaryaddition(final).asm
+++ binaryaddition(final).asm
@@ -67,3 +67,79 @@ loop print_subtraction
 main endp
 
 end main
+
+
+.model small
+.stack 100h
+
+.data
+    num1 db 11111b         ; Binary: 11111 (decimal 31)
+    num2 db 10110b         ; Binary: 10110 (decimal 22)
+    
+.code
+
+main proc
+    ; Initialize data segment
+    mov ax, @data
+    mov ds, ax   
+
+    ; --------------------------
+    ; Binary Addition
+    ; --------------------------
+    mov al, num2           ; Load num2 (10110b) into AL
+    mov bl, num1           ; Load num1 (11111b) into BL
+    add bl, al             ; Add AL and BL, store result in BL
+    
+    mov cx, 8              ; Prepare to print 8 bits (binary format)
+print_addition: 
+    mov ah, 2              ; DOS function to print character
+    mov dl, '0'            ; Default character is '0'
+    test bl, 10000000b      ; Test the leftmost (MSB) bit
+    jz zero_addition        ; If zero, skip setting DL to '1'
+    mov dl, '1'            ; Set DL to '1' if MSB is set
+zero_addition:  
+    int 21h                ; Print the character (either '0' or '1')
+    shl bl, 1              ; Shift BL left to check the next bit
+loop print_addition         ; Repeat until all 8 bits are printed
+
+    ; Print binary suffix to indicate binary number
+    mov dl, 'b'            ; Print the 'b' suffix
+    int 21h                ; Call DOS interrupt
+
+    ; New line after binary addition result
+    mov dl, 0Dh            ; Carriage return (to move to the beginning of the next line)
+    int 21h                ; DOS interrupt for carriage return
+    mov dl, 0Ah            ; Line feed (to move to the next line)
+    int 21h                ; DOS interrupt for line feed
+
+    ; --------------------------
+    ; Binary Subtraction
+    ; --------------------------
+    mov al, num2           ; Load num2 (10110b) into AL
+    mov bl, num1           ; Load num1 (11111b) into BL
+    sub bl, al             ; Subtract AL from BL, store result in BL
+
+    mov cx, 8              ; Prepare to print 8 bits (binary format)
+print_subtraction: 
+    mov ah, 2              ; DOS function to print character
+    mov dl, '0'            ; Default character is '0'
+    test bl, 10000000b      ; Test the leftmost (MSB) bit
+    jz zero_subtraction     ; If zero, skip setting DL to '1'
+    mov dl, '1'            ; Set DL to '1' if MSB is set
+zero_subtraction:  
+    int 21h                ; Print the character (either '0' or '1')
+    shl bl, 1              ; Shift BL left to check the next bit
+loop print_subtraction      ; Repeat until all 8 bits are printed
+
+    ; Print binary suffix to indicate binary number
+    mov dl, 'b'            ; Print the 'b' suffix
+    int 21h                ; Call DOS interrupt
+
+    ; --------------------------
+    ; Exit the program
+    ; --------------------------
+    mov ah, 4Ch            ; DOS function to terminate program
+    int 21h                ; Call DOS interrupt to exit
+main endp
+
+end main
--- logical(AND,OR,NOT,XOR)final.asm
+++ logical(AND,OR,NOT,XOR)final.asm
@@ -1,100 +1,97 @@
-.model small
-.stack 100h
+.model small        ; Define a small memory model
+.stack 100h         ; Reserve 256 bytes for the stack
 .data
-a db 'for AND :$'
-b db 'for OR  :$'
-c db 'for XOR :$'
-d db 'for NOT :$'
+a db 'for AND :$'   ; Define string for AND operation output
+b db 'for OR  :$'   ; Define string for OR operation output
+c db 'for XOR :$'   ; Define string for XOR operation output
+d db 'for NOT :$'   ; Define string for NOT operation output
 .code
 main proc
-    mov ax, @data
-    mov ds, ax
-    
+    ; Set up data segment
+    mov ax, @data   ; Load the address of data segment into AX
+    mov ds, ax      ; Move the content of AX into DS to set up data segment
+
     ; AND Operation
     and1:
-    mov ah, 9
-    lea dx, a
-    int 21h
-    
-    mov bl, 111b
-    and bl, 101b
-    add bl, 48  ; Convert the result to ASCII (only works if result is 0 or 1)
-    
-    mov ah, 2
-    mov dl, bl
-    int 21h   
-    
-    
-    mov ah,2
-    mov dl,10
-    int 21h
-    mov dl,13
-    int 21h
-    
+    mov ah, 9       ; Function 9h (DOS interrupt for displaying a string)
+    lea dx, a       ; Load the address of string 'a' into DX
+    int 21h         ; Interrupt 21h to print string
+
+    mov bl, 111b    ; Load binary 111 into BL
+    and bl, 101b    ; Perform bitwise AND with 101b
+    add bl, 48      ; Convert the result (0 or 1) into ASCII by adding 48
+
+    mov ah, 2       ; Function 2h (DOS interrupt for printing a character)
+    mov dl, bl      ; Load the result in DL
+    int 21h         ; Interrupt 21h to print the character (result of AND)
+
+    ; Print newline (CR + LF)
+    mov ah, 2       ; Prepare for another character output
+    mov dl, 10      ; ASCII code for newline (LF)
+    int 21h         ; Print newline (LF)
+    mov dl, 13      ; ASCII code for carriage return (CR)
+    int 21h         ; Print carriage return (CR)
+
     ; OR Operation
     or1:
-    mov ah, 9
-    lea dx, b
-    int 21h
-    
-    mov bl, 111b
-    or bl, 101b
-    add bl, 48  ; Convert the result to ASCII (only works if result is 0 or 1) 
-    
-    
+    mov ah, 9       ; Function 9h to display string
+    lea dx, b       ; Load the address of string 'b' into DX
+    int 21h         ; Interrupt 21h to print the string
+
+    mov bl, 111b    ; Load binary 111 into BL
+    or bl, 101b     ; Perform bitwise OR with 101b
+    add bl, 48      ; Convert the result (0 or 1) into ASCII
+
+    mov ah, 2       ; Prepare to print the result
+    mov dl, bl      ; Load the result in DL
+    int 21h         ; Interrupt 21h to print the character (result of OR)
+
+    ; Print newline (CR + LF)
     mov ah, 2
-    mov dl, bl
-    int 21h     
-    
-    mov ah,2
-    mov dl,10
+    mov dl, 10      ; Newline (LF)
     int 21h
-    mov dl,13
+    mov dl, 13      ; Carriage return (CR)
     int 21h
-    
+
     ; XOR Operation
     xor1:
-    mov ah, 9
-    lea dx, c
-    int 21h
-    
-    
-    
-    
-    mov bl, 111b
-    xor bl, 101b
-    add bl, 48  ; Convert the result to ASCII (only works if result is 0 or 1)
-    
-    mov ah, 2
-    mov dl, bl
-    int 21h 
-    
-      
-    mov ah,2
-    mov dl,10
-    int 21h
-    mov dl,13
-    int 21h   
+    mov ah, 9       ; Function 9h to display string
+    lea dx, c       ; Load the address of string 'c' into DX
+    int 21h         ; Interrupt 21h to print the string
 
-    
-    
+    mov bl, 111b    ; Load binary 111 into BL
+    xor bl, 101b    ; Perform bitwise XOR with 101b
+    add bl, 48      ; Convert the result (0 or 1) into ASCII
+
+    mov ah, 2       ; Prepare to print the result
+    mov dl, bl      ; Load the result in DL
+    int 21h         ; Interrupt 21h to print the character (result of XOR)
+
+    ; Print newline (CR + LF)
+    mov ah, 2
+    mov dl, 10      ; Newline (LF)
+    int 21h
+    mov dl, 13      ; Carriage return (CR)
+    int 21h
+
     ; NOT Operation
     not1:
-    mov ah, 9
-    lea dx, d
-    int 21h
-    
-    mov bl, 11000000b
-    not bl            ; Perform the NOT operation (negates the entire register)
-    and bl, 00000011b ; Mask to get the last two bits (since BL is 8 bits wide)
-    add bl, 48        ; Convert to ASCII
-    
-    mov ah, 2
-    mov dl, bl
-    int 21h
-    
+    mov ah, 9       ; Function 9h to display string
+    lea dx, d       ; Load the address of string 'd' into DX
+    int 21h         ; Interrupt 21h to print the string
+
+    mov bl, 11000000b ; Load binary 11000000 into BL
+    not bl            ; Perform bitwise NOT (invert all bits of BL)
+    and bl, 00000011b ; Mask with 00000011b to isolate the last two bits
+    add bl, 48        ; Convert the result (0 or 1) into ASCII
+
+    mov ah, 2         ; Prepare to print the result
+    mov dl, bl        ; Load the result in DL
+    int 21h           ; Interrupt 21h to print the character (result of NOT)
+
+    ; Exit program
     exit:
-    mov ah, 4ch
-    int 21h
+    mov ah, 4ch      ; Function 4Ch to exit program
+    int 21h          ; Interrupt 21h to terminate the program
 main endp
 end main
